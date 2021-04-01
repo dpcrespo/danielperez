780f89190e943b298a0d5e2f7885d89f3e6a45be
dperez <tcoldpc@gmail.com>
2021-04-01 22:33:40 +0200

    inclued navbar with position sticky

--- src/App.js
+++ src/App.js
@@ -2,36 +2,26 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="container">
-      <header className="header">
-        <span className="logo">DPC</span>
-        <div className="navbar">
-          <a href="#about" className="link">About</a>
-          <a href="#skills" className="link">Skills</a>
-          <a href="#experience" className="link">Experience</a>
-          <a href="#education" className="link">Education</a>
-        </div>
+    <>
+      <header>
+        <span>DPC</span>
+        <nav>
+          <ul className="nav-links">
+            <li><a href="#about">About</a></li>
+            <li><a href="#skills">Skills</a></li>
+            <li><a href="#experience">Experience</a></li>
+            <li><a href="#education">Education</a></li>
+          </ul>
+        </nav>
       </header>
-      <div className="main">
-        <div id="about" className="section-shape">
-          <h1>About</h1>
-          <a href="#skills" className="next">Next</a>
-        </div>
-        <div id="skills" className="section-shape">
-          <h1>Skills</h1>
-          <a href="#experience">Next</a>
-        </div>
-        <div id="experience" className="section-shape">
-          <h1>Experience</h1>
-          <a href="#education">Next</a>
-        </div>
-        <div id="education" className="section-shape">
-          <h1>Education</h1>
-          <a href="#about">Next</a>
-        </div>
-      </div>
-      
-    </div>
+      <section id="about">
+        <h1>About</h1>
+        <p>hola</p>
+      </section>
+      <section id="skills">
+        <h1>Skills</h1>
+      </section>
+    </>
   );
 }
 
