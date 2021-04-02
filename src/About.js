99646b66763d190b9256905d4add17e0c08df04e
dperez <tcoldpc@gmail.com>
2021-04-02 14:57:12 +0200

    fixed paragraph

--- src/About.js
+++ src/About.js
@@ -19,7 +19,7 @@ export const About = () =>
             <article>
                 <p>
                     I am a person who is passionate about learning new technologies and applying them in every project that I have participated in. One of my motivations is to create things from scratch to achieving a product or services that is successful for the people. My experience and my knowledge was gained throughout 8 years. Firstly, I was working 3 years as system administrator, then I started working in as healthcare consultant during 1 year, after that I had the opportunity to progress to a healthcare project manager where I have been involved almost three years. Lately I have giving my time between web project manager and web software developer.
-            </p>
+                </p>
             </article>
         </div>
     </section>
